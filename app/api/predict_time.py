65cb8b62a6b723f84fea2a0951beeca53f76bee7
Tekalig <tekahazi06@gmail.com>
2025-05-10 22:53:42 +0300

    Refactor predict_time endpoint to improve error handling and streamline car detection process

--- app/api/predict_time.py
+++ app/api/predict_time.py
@@ -1,32 +1,61 @@
 from fastapi import APIRouter, UploadFile, HTTPException
-from app.core.detection_service import DetectionService
-from app.core.prediction_service import PredictionService
+from fastapi.logger import logger
 import cv2
 import numpy as np
+from app.core.detection_service import DetectionService
+from app.core.passing_time import signal_controller_cycle
+
 
 router = APIRouter()
 
-detection_service = DetectionService("app/models/car_detection/model.pth")
-prediction_service = PredictionService("app/models/passing_time_ann/model.h5")
+detection_service = DetectionService("app/models/yolo11n.pt")
+
 
 @router.post("/api/predict-time")
 async def predict_time(file: UploadFile):
-    if not file.content_type.startswith("image/"):
-        raise HTTPException(status_code=400, detail="Invalid file type. Please upload an image.")
+    logger.info(f"Received file: {file.filename}, Content-Type: {file.content_type}")
+    if not file.content_type or not file.content_type.startswith("image/"):
+        raise HTTPException(
+            status_code=400, detail="Invalid file type. Please upload an image."
+        )
 
     try:
         # Read the image file
         image_bytes = await file.read()
+        logger.info(f"Size of uploaded file in bytes: {len(image_bytes)}")
+        if not image_bytes:
+            raise ValueError("Uploaded file is empty.")
+
+        # Decode the image
         image = np.frombuffer(image_bytes, np.uint8)
         image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+        if image is None:
+            raise ValueError(
+                "Failed to decode image. Ensure the file is a valid image."
+            )
+
+        logger.info(f"Image successfully decoded. Shape: {image.shape}")
+
+        # image = preprocess_image_for_yolo(image)
 
         # Detect cars
         car_count = detection_service.detect_cars(image)
 
-        # Predict passing time
-        estimated_time = prediction_service.predict_passing_time(car_count)
+        # Convert Counter keys to strings or ints
+        car_count_json_safe = dict(car_count)
 
-        return {"car_count": car_count, "estimated_passing_time_seconds": estimated_time}
+        # call signal_controller_cycle
+        pass_time = signal_controller_cycle(car_count_json_safe)
 
+        return {
+            "car_count": car_count_json_safe,
+            "estimated_passing_time_seconds": pass_time,
+        }
+
+    except ValueError as ve:
+        logger.error(f"Validation error: {ve}")
+        raise HTTPException(status_code=400, detail=str(ve))
     except Exception as e:
+        logger.error(f"Error processing the image: {e}")
         raise HTTPException(status_code=500, detail=f"Error processing the image: {e}")
