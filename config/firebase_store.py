a0f2debf7108984aed7cbcc8a34c6d937bef4f0e
Tekalig <tekahazi06@gmail.com>
2025-06-14 16:04:59 +0300

    Refactor firebase_store.py to replace print statements with logging for improved error handling and information tracking

--- config/firebase_store.py
+++ config/firebase_store.py
@@ -1,6 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 import json
+import logging
 
 # Load the service account key from the JSON file
 with open(".env/traffic-data.json", "r") as key_file:
@@ -19,8 +20,8 @@ def store_data(collection_name, document_id, data):
     try:
         doc_ref = db.collection(collection_name).document(document_id)
         doc_ref.set(data)
-        print(
+        logging.info(
             f"Document {document_id} successfully written to {collection_name} collection."
         )
     except Exception as e:
-        print(f"An error occurred: {e}")
+        logging.error(f"An error occurred: {e}")
